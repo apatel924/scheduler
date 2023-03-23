245e9d4ef9738c2cfd004e8cae4999e5101fb684
apatel924 <apatel2@ualberta.ca>
2023-03-23 12:11:37 -0600

    added interviewerlist and listitem files as well as code for lists and added sample data to storries-index js file

--- stories/index.js
+++ stories/index.js
@@ -8,7 +8,8 @@ import "index.scss";
 import Button from "../src/components/Button";
 import DayListItem from "../src/components/DayListItem";
 import DayList from '../src/components/DayList';
-import Button from "components/Button";
+import InterviewerListItem from "components/InterviewerListItem";
+import InterviewerList from "components/InterviewerList";
 
 
 // Button
@@ -40,36 +41,96 @@ storiesOf("DayListItem", module)
     <DayListItem name="Tuesday" onChange={action("setDay")} spots={5} />
   ));
 
-//Day List - Dummy Data
-const days = [
-  {
+  const interviewer = {
     id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+    name: "Sylvia Palmer",
+    avatar: "https://i.imgur.com/LpaY82x.png",
+  };
+  
+  //Day List - Dummy Data
+  const days = [
+    {
+      id: 1,
+      name: "Monday",
+      spots: 2,
+    },
+    {
+      id: 2,
+      name: "Tuesday",
+      spots: 5,
+    },
+    {
+      id: 3,
+      name: "Wednesday",
+      spots: 0,
+    },
+  ];
+  
+  //Day List
+  storiesOf("DayList", module)
+    .addParameters({
+      backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
+    })
+    .add("Monday", () => (
+      <DayList days={days} value={"Monday"} onChange={action("setDay")} />
+    ))
+    .add("Tuesday", () => (
+      <DayList days={days} value={"Tuesday"} onChange={action("setDay")} />
+    ))
+    .add("Wednesday", () => (
+      <DayList days={days} value={"Wednesday"} onChange={action("setDay")} />
+    ));
 
-//Day List
-storiesOf("DayList", module)
-  .addParameters({
-    backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
-  })
-  .add("Monday", () => (
-    <DayList days={days} value={"Monday"} onChange={action("setDay")} />
-  ))
-  .add("Tuesday", () => (
-    <DayList days={days} value={"Tuesday"} onChange={action("setDay")} />
-  ))
-  .add("Wednesday", () => (
-    <DayList days={days} value={"Wednesday"} onChange={action("setDay")} />
-  ));
+  // Interviewer list items
+  storiesOf("InterviewerListItem", module)
+    .addParameters({
+      backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
+    })
+    .add("Unselected", () => (
+      <InterviewerListItem
+        id={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+      />
+    ))
+    .add("Selected", () => (
+      <InterviewerListItem
+        id={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected
+      />
+    ))
+    .add("Clickable", () => (
+      <InterviewerListItem
+        id={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        setInterviewer={() => action("setInterviewer")(interviewer.id)}
+      />
+    ));
+  
+  // dummy data of interviewers
+  const interviewers = [
+    { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+    { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+    { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+    { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+    { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" },
+  ];
+  
+  // interviewer list
+  storiesOf("InterviewerList", module)
+    .addParameters({
+      backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
+    })
+    .add("Initial", () => <InterviewerList interviewers={interviewers} />)
+    .add("Selected", () => (
+      <InterviewerList interviewers={interviewers} value={3} />
+    ))
+    .add("Clickable", () => (
+      <InterviewerList
+        interviewers={interviewers}
+        setInterviewer={() => action("onChange")(interviewer.id)}
+      />
+    ));
