3c953a2ffa160aef7457b53842a8bf43850deeab
apatel924 <apatel2@ualberta.ca>
2023-03-24 08:00:24 -0600

    added form component and updated form for edit and create

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,4 +3,25 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Empty from "./Empty";
 import Show from "./Show";
+import Confirm from "./Confirm";
+import Status from "./Status";
+import Error from "./Error";
+import Form from "./Form";
 
+export default function Appointment(props) {
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer.name}
+          onEdit={props.onEdit}
+          onDelete={props.onDelete}
+        />
+      ) : (
+        <Empty onAdd={props.onAdd} />
+      )}
+    </article>
+  );
+}
