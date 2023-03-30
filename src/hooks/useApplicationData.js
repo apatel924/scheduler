0b5e83f5ca586471087400a51a9a7de1fd49a042
ashish <apatel2@ualberta.ca>
2023-03-30 12:09:29 +0000

    added extra tests for appointment and test fixed some routing issues and database issues as well as some parts of code that were buggy

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import Application from "../components/Application";
+import axios from "axios";
 
 export default function useApplicationData() {
   const [state, setState] = useState({
